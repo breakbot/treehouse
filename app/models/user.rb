3d0145149a8cb2bc84e89c5722fad220c646b360
darren logue <cockasucka6@gmail.com>
2013-07-07 15:25:57 +0100

    tennis

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,16 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :first_name, :last_name, :profile_name, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
-  
+
+  validates :first_name,    presence: true
+  validates :last_name,     presence: true
+  validates :profile_name,  presence: true,
+                            uniqueness: true,
+                            format: {
+                              with: /a-zA-Z0-9_-/,
+                              message: 'must be correctly formatted'
+                            }
+
   has_many :statuses
 
   def full_name
